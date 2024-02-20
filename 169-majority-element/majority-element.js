ccd7dcbc2207b443bb296fac5c7b17da93986ffb
kichopher <66693006+kichopher@users.noreply.github.com>
2024-02-20 22:58:23 +0530

    Time: 58 ms (65.75%) | Memory: 51.1 MB (38.64%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,25 +3,23 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    // Approach 1: NlogN time and constant space -> sort the array and check which number appears in more that half (from start or end)
-    /** Approach 2: 
-    Create a Map (not using object because keys can't be number in js-object) to keep track of count of each unique number
-    After traversing nums, and updating the number-count Map, traverse the number-count Map to find the number with count > n/2
-    Time complexity: O(n) [array traversal]
-    Space complexity: O(1) [Since 1<= n <= 5 * 10^4, There will only be atmost 5*10^4 unique numbers, so the Map will have a size proportional to to 5*10^4 atmost]
-     */
-    const numCountMap = new Map();
-    nums.forEach(num=>{
-        const currentNumCount = numCountMap.get(num); // returns undefined if num not present
-        if (!currentNumCount){
-            numCountMap.set(num, 1)
-        }else{
-            numCountMap.set(num, currentNumCount + 1);
+    // implement Boyer-Moore algorithm for O(n) time and O(1) space
+    let majorityElement = nums[0];
+    let count = 1;
+    // find candidate
+    for(let i = 1; i< nums.length; i++ ){
+        if (nums[i] !== majorityElement) count--;
+        else count++;
+
+        if (count === 0) {
+            majorityElement = nums[i];
+            count=1;
         }
-    })
-    let majorityElement;
-    numCountMap.forEach((value, num)=> {
-        if (value > (nums.length/2)) majorityElement = num;
-    })
-    return majorityElement;
+    }
+    // verify if candidate is majorityElement
+    let candidateCount = 0;
+    for(let i = 0; i< nums.length; i++){
+        if (nums[i] === majorityElement) candidateCount++;
+    }
+    return (candidateCount > (nums.length/2)) ? majorityElement : null;
 };

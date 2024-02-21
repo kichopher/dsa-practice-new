ee2c76487c1e3a7f9e4dc4b87234893e5546f21e
kichopher <66693006+kichopher@users.noreply.github.com>
2024-02-21 23:32:32 +0530

    Time: 89 ms (32.59%) | Memory: 59.8 MB (17.22%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -3,16 +3,26 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function(nums, k) {
-    // approach-1 O(n) space and time, create an temp-array to arrange all numbers in the correct order and finally copy values to the nums array
-    const tempArray = [];
-    tempArray.length = nums.length;
-    for (let i=0; i<nums.length; i++){
-        const targetIndex = (i + k) % nums.length;
-        tempArray[targetIndex] = nums[i];
+var rotate = function (nums, k) {
+    // O(n) time and O(1) space approach:
+    // 1. Reverse the entire array
+    // 2. Reverse the first k elements
+    // 3. Reverse the remaining elements
+    function reverseArrayRange(arr, startIx, endIx) {
+        const rangeLength = endIx - startIx + 1;
+        const numberOfMovesRequired = Math.trunc(rangeLength/2);
+        for (let i = 0; i < numberOfMovesRequired; i++) {
+            const temp = arr[startIx + i];
+            arr[startIx + i] = arr[endIx - i];
+            arr[endIx - i] = temp;
+        }
     }
-    // now copy values from tempArray to nums array
-    for (let i=0; i<nums.length; i++){
-        nums[i] = tempArray[i];
-    }  
+    const kActual = k % nums.length; // this corrects for when k >= nums.length
+    if (kActual === 0) return; // this removes the edge cases that can mess up steps 2 and 3
+    /** 1 */
+    reverseArrayRange(nums, 0, nums.length - 1);
+    /** 2 */
+    reverseArrayRange(nums, 0, kActual - 1);
+    /** 3 */
+    reverseArrayRange(nums, kActual, nums.length - 1);
 };

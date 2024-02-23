9f05fc13e23c73506ddb4e0cb627c07826d326db
kichopher <66693006+kichopher@users.noreply.github.com>
2024-02-23 07:47:30 +0530

    Time: 66 ms (88.61%) | Memory: 58.4 MB (54.22%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,8 +3,7 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-    // Approach 1: Brute force: for-each number, find the difference with a later number
-    // O(n2) time
+    // Optimal approach: Greedy algorithm. O(n) time and O(1) space
     let maxProfit = 0;
     let buyPrice = prices[0];
     for (let i = 1; i < prices.length; i++) {

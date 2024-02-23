76c38a6088047ebeb3ca7f5f73ec6d51fdd9142d
kichopher <66693006+kichopher@users.noreply.github.com>
2024-02-23 08:27:59 +0530

    Time: 55 ms (63.17%) | Memory: 48.1 MB (58.22%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
+    // Greedy algorithm. O(n) time & O(1) space
     let buyPrice = prices[0];
     let profit = 0;
     for (let i=1; i< prices.length; i++){
         const currentPrice = prices[i];
         if (currentPrice > buyPrice){
             profit+= currentPrice - buyPrice;
-            
         }
         buyPrice = currentPrice;
     }

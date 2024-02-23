c22dfb474728862e0a08b67e3dfd7867851c33dd
kichopher <66693006+kichopher@users.noreply.github.com>
2024-02-23 22:22:34 +0530

    Time: 60 ms (54.84%) | Memory: 50.8 MB (50.69%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.js
+++ 45-jump-game-ii/jump-game-ii.js
@@ -3,7 +3,9 @@
  * @return {number}
  */
 var jump = function (nums) {
-    let jumps = 0, currentJumpEnd = 0, farthest = 0;
+    let jumps = 0;
+    let farthest = 0;
+    let currentJumpEnd = 0;
     for (let i = 0; i < nums.length - 1; i++) {
         farthest = Math.max(farthest, i + nums[i]);
         if (i === currentJumpEnd) {

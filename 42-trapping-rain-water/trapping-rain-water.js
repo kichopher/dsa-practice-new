27a25c04dbeeccc3f862bab747d5501557de6424
kichopher <66693006+kichopher@users.noreply.github.com>
2024-03-03 23:45:44 +0530

    Time: 60 ms (59.86%) | Memory: 51.3 MB (35.35%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function (height) {
+    let left = 0, right = height.length - 1;
+    let maxLeft = 0, maxRight = 0;
+    let result = 0;
+
+    while (left < right) {
+        if (height[left] <= height[right]) {
+            if (height[left] >= maxLeft) {
+                maxLeft = height[left];
+            } else {
+                result += maxLeft - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] >= maxRight) {
+                maxRight = height[right];
+            } else {
+                result += maxRight - height[right];
+            }
+            right--;
+        }
+    }
+
+    return result;
+};

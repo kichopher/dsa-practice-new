02b629c8d3941f616e46abefe1edf61a27900de1
kichopher <66693006+kichopher@users.noreply.github.com>
2024-03-23 17:05:59 +0530

    Time: 50 ms (71.01%) | Memory: 50.1 MB (8.02%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,6 +4,33 @@
  * @return {number}
  */
 var strStr = function (haystack, needle) {
-    const i = haystack.indexOf(needle) ?? -1
-    return i;
+    /** Solving with Booyer Moore Algorith 
+        Worst case time complexity is O(mn) but average time complexity is O(n)
+     */
+    // preprocess needle (record last occurence of each character in needle)
+    const last = {};
+    for (let i = 0; i < needle.length; i++) last[needle[i]] = i;
+
+    let haystack_start_index = 0;
+    while (haystack_start_index + needle.length - 1 <= haystack.length - 1) {
+        /** start comparing from the end
+         If mismatch found, re-calculate haystack_start_index  */
+        for (let i = needle.length - 1; i >= 0; i--) {
+            const hp = haystack_start_index + i
+            if (i === 0 && needle[i] === haystack[hp]) return haystack_start_index;
+            if (needle[i] !== haystack[hp]) {
+                /** determine the new haystack_start_index */
+                if (last.hasOwnProperty(haystack[hp])) {
+                    const lastNeedleCharIndex = last[haystack[hp]];
+                    const moves = i - lastNeedleCharIndex
+                    haystack_start_index += Math.max(1, moves);
+                    break;
+                }
+                /** else skip till past this index */
+                haystack_start_index = hp + 1;
+                break;
+            }
+        }
+    }
+    return -1;
 }

0fe54c632c964163b6de96de6355206a532c9563
kichopher <66693006+kichopher@users.noreply.github.com>
2024-08-05 20:41:04 +0530

    Time: 71 ms (33.56%) | Memory: 51.4 MB (95.54%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,18 +3,13 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    // duplicates will occur together in a sorted array
-    const numsLength = nums.length;
-    if (numsLength <= 1) return numsLength;
-    // start from the second element
-    let currentUniqueNumberIndex = 0;
-    for (let i=1; i< numsLength; i++) {
-        if (nums[currentUniqueNumberIndex] === nums[i]){
-            continue;
-        }else {
-            currentUniqueNumberIndex++;
-            nums[currentUniqueNumberIndex] = nums[i];
+    let uip = 0; // unique index pointer
+    for (let i = 1; i < nums.length; i++){
+        if (nums[uip]!==nums[i]){
+            // unique number found
+            uip += 1;
+            nums[uip] = nums[i];
         }
     }
-    return currentUniqueNumberIndex + 1;
+    return uip+1;
 };

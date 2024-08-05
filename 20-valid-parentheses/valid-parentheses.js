9440d76ef53a42e7a7ea3c8fd123fb48acdd0983
kichopher <66693006+kichopher@users.noreply.github.com>
2024-08-05 22:18:45 +0530

    Time: 112 ms (5.00%) | Memory: 55.8 MB (5.09%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    const setOfClosingBraces = new Set([")", "}", "]"]);
+    const mapOfOpeningBracesToTheirClosingBraces = new Map();
+    mapOfOpeningBracesToTheirClosingBraces.set("(", ")");
+    mapOfOpeningBracesToTheirClosingBraces.set("{", "}");
+    mapOfOpeningBracesToTheirClosingBraces.set("[", "]");
+    const stackOfOpeningBraces = [];
+    for (i = 0; i < s.length; i++) {
+        const char = s[i];
+        console.log(char)
+        if (mapOfOpeningBracesToTheirClosingBraces.has(char)) {
+            // is opening bracket
+            stackOfOpeningBraces.push(char)
+        } else {
+            // is closing bracket (find pair in stack)
+            const poppedOpeningBracket = stackOfOpeningBraces.pop();
+            if (mapOfOpeningBracesToTheirClosingBraces.get(poppedOpeningBracket) === char) {
+                continue;
+            } else { return false; }
+        }
+    }
+    console.log(stackOfOpeningBraces)
+    return stackOfOpeningBraces.length ? false : true;
+};

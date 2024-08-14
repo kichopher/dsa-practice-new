0f873ac588dce77a8862df4f1c763dcbe7eef587
kichopher <66693006+kichopher@users.noreply.github.com>
2024-08-14 09:15:55 +0530

    Time: 76 ms (14.40%) | Memory: 53.3 MB (45.36%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -3,18 +3,27 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-    const matchedChars = s.match(/[A-Z0-9]/gi) || []; // could be empty if no match found
-    const processedString = matchedChars.reduce((acc, curr) => (acc + curr.toLowerCase()), "");
-    // console.log([processedString])
-    let sp = 0;
-    let ep = processedString.length - 1;
-    while (sp < ep) {
-        if (processedString[sp] === processedString[ep]) {
-            sp++; ep--;
-            continue;
-        } else {
+    let t = "";
+    for (let i = 0; i < s.length; i++) {
+        const currCharCode = s.charCodeAt(i);
+        const charCodeSmallA = "a".charCodeAt(0);
+        const charCodeSmallZ = "z".charCodeAt(0);
+        const charCodeBigA = "A".charCodeAt(0);
+        const charCodeBigZ = "Z".charCodeAt(0);
+        if (currCharCode >= charCodeSmallA && currCharCode <= charCodeSmallZ) {
+            t += s[i];
+        } else if (currCharCode >= charCodeBigA && currCharCode <= charCodeBigZ) {
+            t += s[i].toLowerCase();
+        } else if (currCharCode >= "0".charCodeAt(0) && currCharCode <= "9".charCodeAt(0)) {
+            t += s[i];
+        }
+    }
+    let [l, r] = [0, t.length - 1];
+    while (l < r) {
+        if (t[l] !== t[r]) {
             return false;
         }
+        l++; r--;
     }
     return true;
 };

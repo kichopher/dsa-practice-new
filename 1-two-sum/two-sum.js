addfbeb625c494d393b0b4c0afc03b5d92818e99
kichopher <66693006+kichopher@users.noreply.github.com>
2024-08-25 10:20:47 +0530

    Time: 61 ms (65.67%) | Memory: 52.1 MB (9.50%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,16 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    // Sol 1: time O(n2) space O(1): Have one pointer that takes one step after the other pointer has finished traversing the values to the right of the first pointer.
-    // Sol 2: time O(nLogn) space O(n): Create an array or arrays which each element in the array being a tuple of value and original-index. Then sort the arrays based on value at index-0. Now find the right sum with two pointers at either end. Return the indeces.
-    // Sol 3: time O(n) space O(n): Maintain a hashMap of seen values and indeces, and look for target-num in the hashMap 
-    const seenNumIndeces = new Map();
+    const seenNumberIndex = new Map();
     for (let i = 0; i< nums.length; i++){
-        const num = nums[i];
-        const diff = target - num;
-        if (seenNumIndeces.has(diff)){
-            return [seenNumIndeces.get(diff), i]
+        const curr = nums[i];
+        const req = target - curr;
+        if (seenNumberIndex.has(req)){
+            return [seenNumberIndex.get(req), i]
+        }else {
+            seenNumberIndex.set(curr, i)
         }
-        seenNumIndeces.set(num, i)
     }
 };

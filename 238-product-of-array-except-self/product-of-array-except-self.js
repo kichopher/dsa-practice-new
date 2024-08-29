439ca8c0659166fb0810c4fe7372c639908b4c8b
kichopher <66693006+kichopher@users.noreply.github.com>
2024-08-29 08:54:14 +0530

    Time: 105 ms (51.82%) | Memory: 61.3 MB (94.00%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -3,21 +3,22 @@
  * @return {number[]}
  */
 var productExceptSelf = function (nums) {
-    // O(n) time and O(n) memory solution: Calculate prefix array and postfix array and then multiply
-    const prefixes = new Array(nums.length).fill(1); // set default value 1 (1*any = any)
-    const postfixes = new Array(nums.length).fill(1);
+    /** O(n) time and O(1) memory solution: 
+    Calculate and insert prefix in result array first 
+    and then calculate postfix, calculate product with prefix and store at each index */
+
+    const result = new Array(nums.length).fill(1); // set default value 1 (1*any = any)
 
     // calculate prefix for each index (no prefix for index 0):
     for (let i = 1; i < nums.length; i++) {
-        prefixes[i] = prefixes[i - 1] * nums[i - 1];
+        result[i] = result[i - 1] * nums[i - 1];
     }
-
     // calculate postfix for each index (no postfix for last index):
+    let postfix = 1;
     for (let i = nums.length - 2; i >= 0; i--) {
-        postfixes[i] = postfixes[i + 1] * nums[i + 1];
+        postfix *= nums[i + 1];
+        result[i] *= postfix;
     }
 
-    const result = [];
-    prefixes.forEach((prefix, i) => { result.push(prefix * postfixes[i]) });
     return result;
 };

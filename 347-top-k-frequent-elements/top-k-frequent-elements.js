22a895918aa7fcd18cddf02fef99af7a3950b214
kichopher <66693006+kichopher@users.noreply.github.com>
2024-09-03 09:31:17 +0530

    Time: 60 ms (87.04%) | Memory: 52 MB (93.70%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -4,20 +4,20 @@
  * @return {number[]}
  */
 var topKFrequent = function (nums, k) {
-    const numFreq = new Map();
+    const numCountMap = new Map()
     nums.forEach(num => {
-        numFreq.set(num, (numFreq.get(num) ?? 0) + 1);
+        numCountMap.set(num, (numCountMap.get(num) ?? 0) + 1)
     })
-    const freqGroupedNums = []; // index is the frequency
-    for (let i = 0; i < nums.length + 1; i++) {
-        freqGroupedNums.push([]);
-    }
-    [...numFreq.entries()].forEach(([num, freq]) => {
-        freqGroupedNums[freq].push(num);
+    const numAtCountIndexArray = new Array(nums.length + 1);
+    [...numCountMap.entries()].forEach(entry => {
+        const [num, freq] = entry;
+        if (!Array.isArray(numAtCountIndexArray[freq])) numAtCountIndexArray[freq] = []
+        numAtCountIndexArray[freq].push(num);
     })
-    const uniqueNumsSortedByAscendingFreq = [];
-    freqGroupedNums.forEach(group => {
-        uniqueNumsSortedByAscendingFreq.push(...group);
+    const numsInIncreasingOrderOfFreq = []
+    numAtCountIndexArray.forEach(item => {
+        if (Array.isArray(item))
+            numsInIncreasingOrderOfFreq.push(...item);
     })
-    return uniqueNumsSortedByAscendingFreq.slice(-k);
+    return numsInIncreasingOrderOfFreq.slice(-k)
 };

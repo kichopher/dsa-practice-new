cf2d5fcdbd10095bb131dcd779c51cfb3e7694a7
kichopher <66693006+kichopher@users.noreply.github.com>
2024-09-04 09:15:25 +0530

    Time: 96 ms (81.28%) | Memory: 61.8 MB (88.40%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -3,22 +3,16 @@
  * @return {number[]}
  */
 var productExceptSelf = function (nums) {
-    /** O(n) time and O(1) memory solution: 
-    Calculate and insert prefix in result array first 
-    and then calculate postfix, calculate product with prefix and store at each index */
-
-    const result = new Array(nums.length).fill(1); // set default value 1 (1*any = any)
-
-    // calculate prefix for each index (no prefix for index 0):
+    const answer = new Array(nums.length).fill(1);
+    // calculate left product starting index 1
     for (let i = 1; i < nums.length; i++) {
-        result[i] = result[i - 1] * nums[i - 1];
+        answer[i] = answer[i - 1] * nums[i - 1]
     }
-    // calculate postfix for each index (no postfix for last index):
-    let postfix = 1;
+    // calculate right product starting at index len-2
+    let rightProduct = 1;
     for (let i = nums.length - 2; i >= 0; i--) {
-        postfix *= nums[i + 1];
-        result[i] *= postfix;
+        rightProduct = rightProduct * nums[i + 1]
+        answer[i] *= rightProduct
     }
-
-    return result;
+    return answer;
 };

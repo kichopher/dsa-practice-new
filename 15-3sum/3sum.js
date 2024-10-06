797969c59666fab0f50f31b8429e30b47968b997
kichopher <66693006+kichopher@users.noreply.github.com>
2024-10-06 23:45:37 +0530

    Time: 179 ms (27.01%) | Memory: 66.6 MB (16.04%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -3,30 +3,26 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-    /** With sorting
-    time - O(n^2), space - O(n)
-    https://leetcode.com/problems/3sum/solutions/281302/javascript-with-lots-of-explanatory-comments
-    */
-    const results = [];
-    const target = 0;
-    nums.sort((a, b) => a - b); // sort array
+    const result = []
+    const sortedNums = [...nums].sort((a, b) => (a - b));
     let i = 0;
-    while (i < nums.length - 2) {
-        while (i > 0 && nums[i] === nums[i - 1]) i++;
-        let j = i + 1;
-        let k = nums.length - 1;
+    while ( i < sortedNums.length - 2) {
+        let [j, k] = [i + 1, sortedNums.length - 1];
+        const target = -sortedNums[i];
         while (j < k) {
-            const sum = nums[i] + nums[j] + nums[k];
-            if (sum === target) {
-                results.push([nums[i], nums[j], nums[k]]);
-                while (nums[j + 1] === nums[j]) j++;
-                while (nums[k - 1] === nums[k]) k--;
-                j++; k--;
-            } 
-            else if (sum < target) j++
-            else if (sum > target) k--
+            const currSum = sortedNums[j] + sortedNums[k]
+            if (currSum === target) {
+                result.push([sortedNums[i], sortedNums[j], sortedNums[k]]);
+                j++;
+                while ((j < k) && (sortedNums[j] === sortedNums[j - 1])) j++;
+            } else if (currSum < target) {
+                j++;
+            } else if (currSum > target) {
+                k--;
+            }
         }
         i++;
+        while ((i < sortedNums.length - 2) && (sortedNums[i] === sortedNums[i - 1])) i++;
     }
-    return results;
+    return result;
 };

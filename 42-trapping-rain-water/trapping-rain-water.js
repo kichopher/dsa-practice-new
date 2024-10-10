affe1614718bb4cd9d3ea7fd5fcc7470c84a6bcc
kichopher <66693006+kichopher@users.noreply.github.com>
2024-10-10 23:42:47 +0530

    Time: 65 ms (38.59%) | Memory: 54.7 MB (15.66%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.js
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -2,28 +2,17 @@
  * @param {number[]} height
  * @return {number}
  */
-var trap = function(height) {
-    let [left ,right] = [0, height.length - 1];
-    let [maxLeft, maxRight] = [0, 0];
-    let result = 0;
-
-    while (left < right){
-        if (height[left] <= height[right]){
-            if (height[left] >= maxLeft) {
-                maxLeft = height[left];
-            }else{
-                result += maxLeft - height[left]
-            }
-            left++;
-        }else{
-            if (height[right] >= maxRight){
-                maxRight = height[right];
-            }else {
-                result += maxRight - height[right];
-            }
-            right--;
-        }
+var trap = function (height) {
+    const maxToLeft = Array(height.length).fill(0)
+    const maxToRight = Array(height.length).fill(0)
+    for (let i = 1; i < height.length; i++) {
+        const [l, r] = [i, (height.length - 1) - i];
+        maxToLeft[l] = Math.max(maxToLeft[l - 1], height[l - 1])
+        maxToRight[r] = Math.max(maxToRight[r + 1], height[r + 1])
     }
-
-    return result;
+    let water = 0;
+    height.forEach((h, ix)=>{
+        water += Math.max(Math.min(maxToLeft[ix], maxToRight[ix]) - h, 0)
+    })
+    return water;
 };

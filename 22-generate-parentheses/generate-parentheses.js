097ae6cb01af79aa273ae27480142cc0ce78e203
kichopher <66693006+kichopher@users.noreply.github.com>
2024-10-11 16:59:16 +0530

    Time: 52 ms (68.22%) | Memory: 49.9 MB (92.50%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+    const result = [];
+    const growSequence = (openN, closedN, sequence) => {
+        if (closedN === n) {
+            result.push(sequence)
+        } else {
+            // add open-brace
+            if (openN >= closedN && openN < n) {
+                growSequence(openN + 1, closedN, sequence + "(")
+            }
+            // add closed-brace
+            if (openN > closedN) {
+                growSequence(openN, closedN + 1, sequence + ")")
+            }
+        }
+    }
+    growSequence(0, 0, "");
+    return result;
+};

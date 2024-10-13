db8b7b2b9a25ec96eedbdf2d3633fc2388737568
kichopher <66693006+kichopher@users.noreply.github.com>
2024-10-13 13:46:33 +0530

    Time: 202 ms (82.11%) | Memory: 74 MB (41.19%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -4,24 +4,13 @@
  */
 var dailyTemperatures = function (temperatures) {
     const result = new Array(temperatures.length).fill(0);
-    const stack = []; // items are indeces of lesser temperatures
+    const stack = []; // stack of lesser temp-indeces?
     temperatures.forEach((temp, index) => {
-        if (stack.length === 0) {
-            stack.push({ temp, index })
-            return;
+        while (stack.length > 0 && temp > stack[stack.length - 1].temp) {
+            const stackTop = stack.pop();
+            result[stackTop.index] = index - stackTop.index;
         }
-        // else
-        // insert to stack if current temp is less than or equal to temp of top of stack
-        const peekStackTop = () => stack[stack.length - 1]
-        if (temp <= peekStackTop().temp) {
-            stack.push({ temp, index })
-        } else { // temp > peekStackTop
-            while (stack.length && peekStackTop().temp < temp) {
-                const stackTop = stack.pop()
-                result[stackTop.index] = index - stackTop.index
-            }
-            stack.push({ temp, index })
-        }
+        stack.push({ temp, index }); // insert to stack at each step of the iteration
     })
     return result;
 };
